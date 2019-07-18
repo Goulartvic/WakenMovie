67406553b8ebdd2320d97cd7ebbfb90cf1a0b38c
goulart.vic <victor.goulart@agileprocess.com.br>
2019-07-18 14:00:11 -0300

    Create results model Add main activity MVP Loading movie list Show loading icon when loading

--- app/src/main/java/com/tanheta/wakenmovie/base/modules/app_module.kt
+++ app/src/main/java/com/tanheta/wakenmovie/base/modules/app_module.kt
@@ -1,20 +1,31 @@
 package com.tanheta.wakenmovie.base.modules
 
 import com.google.gson.GsonBuilder
+import com.tanheta.wakenmovie.data.feature.main.MainContract
+import com.tanheta.wakenmovie.data.feature.main.MainPresenter
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
 import org.koin.dsl.module.module
 import retrofit2.Converter
 import retrofit2.converter.gson.GsonConverterFactory
 
-val appModule = module {}
+val appModule = module {
+    factory { (view: MainContract.View) ->
+        MainPresenter(
+            view = view,
+            movieRepository = get(),
+            dispatcherContext = get()
+        )
+    } bind MainContract.Presenter::class
+}
 
 val featureModule = module {
     single {
-        GsonConverterFactory.create(GsonBuilder().create())
+        GsonConverterFactory.create(GsonBuilder().setLenient().create())
     } bind Converter.Factory::class
 }
 
 val dispatcherModule = module {
-    factory { Dispatchers.Main as CoroutineDispatcher }
+    factory { Main as CoroutineDispatcher }
 }
--- app/src/main/java/com/tanheta/wakenmovie/data/remote/MovieService.kt
+++ app/src/main/java/com/tanheta/wakenmovie/data/remote/MovieService.kt
@@ -3,11 +3,12 @@ package com.tanheta.wakenmovie.data.remote
 import com.tanheta.wakenmovie.base.extensions.awaitResult
 import com.tanheta.wakenmovie.data.model.ServiceResponse
 import com.tanheta.wakenmovie.data.model.dto.MovieDto
+import com.tanheta.wakenmovie.data.model.dto.ResultsDto
 import com.tanheta.wakenmovie.data.model.dto.SimpleMovieDto
 import kotlinx.coroutines.delay
 
 class MovieService(private val api: MovieApi) : MovieRepository {
-    override suspend fun getUpcomingMovies(page: Long): ServiceResponse<List<SimpleMovieDto>> {
+    override suspend fun getUpcomingMovies(page: Long): ServiceResponse<ResultsDto> {
         return api.getUpcomingMovies(page).awaitResult()
     }
 
