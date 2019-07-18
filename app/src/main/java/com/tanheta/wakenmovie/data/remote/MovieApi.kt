5cbdf171be11d0bdef2c53b7c4ccc662aa0e4a0a
goulart.vic <victor.goulart@agileprocess.com.br>
2019-07-18 14:00:11 -0300

    Create results model Add main activity MVP Loading movie list Show loading icon when loading

--- app/src/main/java/com/tanheta/wakenmovie/data/remote/MovieApi.kt
+++ app/src/main/java/com/tanheta/wakenmovie/data/remote/MovieApi.kt
@@ -2,6 +2,7 @@ package com.tanheta.wakenmovie.data.remote
 
 import com.tanheta.wakenmovie.BuildConfig
 import com.tanheta.wakenmovie.data.model.dto.MovieDto
+import com.tanheta.wakenmovie.data.model.dto.ResultsDto
 import com.tanheta.wakenmovie.data.model.dto.SimpleMovieDto
 import retrofit2.Call
 import retrofit2.http.GET
@@ -9,9 +10,11 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieApi {
-    @GET("/movie/upcoming?" + BuildConfig.TMDB_API_KEY)
-    fun getUpcomingMovies(@Query("page")page: Long): Call<List<SimpleMovieDto>>
+    @GET("movie/upcoming")
+    fun getUpcomingMovies(@Query("page")page: Long,
+                          @Query("api_key")apiKey: String = BuildConfig.TMDB_API_KEY): Call<ResultsDto>
 
-    @GET("/movie/{movie_id}" + BuildConfig.TMDB_API_KEY)
-    fun getMovie(@Path("movie_id") id: Long): Call<MovieDto>
+    @GET("movie/{movie_id}")
+    fun getMovie(@Path("movie_id") id: Long,
+                 @Query("api_key")apiKey: String = BuildConfig.TMDB_API_KEY): Call<MovieDto>
 }
